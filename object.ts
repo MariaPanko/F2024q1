4775965ce76d7cb1723bbc69c43b48bf27e5f704
panko marya <panko marya>
2024-10-11 11:31:25 +0300

    add:machineSpecifications

---
+++ object.ts
@@ -0,0 +1,15 @@
+const machineSpecifications = {
+    title: "Voikswagen Tiguan",
+salon: "dark",
+securitySystem:"ABS",
+pillows: "frot, lateral, rear",
+assistanceSystem: "rain sensor, rear view camera, interns",
+exterior: "легкосплавные диски, рейлинги на крыше",
+headlights: "ксеноновые, противотуманные",
+climte: "климат-контроль",
+heating: "сидений, любового отдела, зеркал, руля",
+multimedia: "CD или MP3, AUX или iPod, bluetooth, мультемедийный экран, штатная новигация",
+comfort: "круиз-контроль"  
+}
+
+console.log(machineSpecifications)
---
+++ veriable.ts
@@ -0,0 +1,4 @@
+const firstnamber=3
+const secontnamber=4
+const result=firstnamber+secontnamber 
+console.log(result)
