5c2c7c30c66bda7d6dd862f402c7f3fd9a556f34
panko marya <panko marya>
2024-10-18 11:31:53 +0300

    add: array

---
+++ array.ts
@@ -0,0 +1,6 @@
+const fruits = ['киви','яблоко','банан','мандарин','манго']
+const vegetables = ['дыня','морковь','картофель','огурец']
+fruits.push('мортышка')
+const plants = fruits.concat(vegetables) 
+plants.sort()
+console.log(plants) 
