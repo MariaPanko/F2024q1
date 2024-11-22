0cf36c5aa7de5fdcc37c359d3706f74b2ff112c6
panko marya <panko marya>
2024-11-22 11:44:47 +0300

    add.sort.js

---
+++ sort.js
@@ -0,0 +1,7 @@
+const words = ['cat', 'sunday','winter', 'happy', 'music']
+const  resultUp = words.sort((a,b)=> a-b)
+//console.log(resultUp)
+
+const beyonce = words.sort((a,b) => a-b)
+const mashalox = beyonce.reverse()
+console.log(beyonce)
